1a87422104ab099fec0bd4c644c42b72a0cf80c8
Runic Elf <perovrsq@gmail.com>
2018-02-24 20:30:15 +0000

    refresh

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('.slider').on('afterChange', (event, slick, currentSlide) => {
-  console.log(currentSlide);
-})
-$('.slider-nav').children()[currentSlide].addClass('slider-nav_elem__active');
+$('.slider-nav_elem1').click(() => $('.slider').slick('slickGoTo', 0));
+$('.slider-nav_elem2').click(() => $('.slider').slick('slickGoTo', 1));
+$('.slider-nav_elem3').click(() => $('.slider').slick('slickGoTo', 2));
+$('.slider-nav_elem4').click(() => $('.slider').slick('slickGoTo', 3));
