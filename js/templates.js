382480f8d47a61e5a740728af9cabd4f8bc2a771
Runic Elf <perovrsq@gmail.com>
2018-03-17 20:41:49 +0000

    ref

---
+++ js/templates.js
@@ -0,0 +1,65 @@
+const header = '\
+        <div class="header_center">\
+            <div class="header_logo">\
+              <a class="header_logo_a" href="/">\
+                <div class="header_logo_img"></div>\
+                <div class="header_logo_text">НФЗМ</div>\
+              </a>\
+            </div>\
+            <div class="header_center_right">\
+                <div class="header_center_right_top">\
+                    <ul class="header_list">\
+                        <li><a class="header_nav_link" href="#">О нас</a></li>\
+                        <li><a class="header_nav_link" href="#">Новости</a></li>\
+                        <li><a class="header_nav_link" href="#">Доставка</a></li>\
+                        <li><a class="header_nav_link" href="#">Гарантии</a></li>\
+                        <li><a class="header_nav_link" href="#">Контакты</a></li>\
+                        <li><a class="header_nav_link" href="#">Вакансии</a></li>\
+                    </ul>\
+                </div>\
+                <div class="header_center_right_bottom">\
+                   <span class="header_center_phone"><i class="fa fa-phone"></i> +7(962)99-989-97</span>\
+                   <span class="header_center_mail"><i class="fa fa-envelope"></i> zakaz@nfzm.ru</span>\
+                </div>\
+            </div>\
+        </div>';
+const footer = '\
+    <div class="footer_wrapper">\
+        <div class="footer_left">\
+         <span class="footer_left_text">© 2018, НФЗМ ВСЕ ПРАВА ЗАЩИЩЕНЫ</span>\
+        </div>\
+        <div class="footer_right">\
+          <div class="footer_right_social">\
+            <a href="#" class="footer_right_social_block footer_right_social_block__vk"></a>\
+            <a href="#" class="footer_right_social_block footer_right_social_block__vk"></a>\
+            <a href="#" class="footer_right_social_block footer_right_social_block__vk"></a>\
+          </div>\
+          <div class="footer_right_contacts">\
+            <span class="footer_right_contacts_text footer_right_contacts_phone">+7 (962) 99-989-97</span>\
+            <span class="footer_right_contacts_text footer_right_contacts_adress">Наро-Фоминск, ул.Парковая д.3</span>\
+            <span class="footer_right_contacts_text footer_right_contacts_email">zakaz@nfzm.ru</span>\
+          </div>\
+        </div>\
+      </div>';
+const undoFooter = '\
+    <div class="undo-footer_wrapper">\
+        <h3 class="undo-footer_h">Заказать металлоконструкцию стало просто!</h3>\
+        <p class="undo-footer_p">\
+          Техническая революция наращивает темпы с каждым годом в геометрической прогрессии.\
+        </p>\
+        <p class="undo-footer_p">\
+          И, вместе с ней, наш завод металлоконструкций интенсивно развивает свои конструкторские компетенции, осваивает новые виды продукции и технологий для того, чтобы удовлетворить ваш спрос на уникальные изделия из металла с повышенными потребительскими свойствами.\
+        </p>\
+        <p class="undo-footer_p">\
+          И если раньше слово «на заказ» означало «трудно выполнимо» и «дорого», то теперь мы ежедневно и с удовольствием реализовываем индивидуальные проекты.\
+        </p>\
+        <p class="undo-footer_p">\
+          Это значит, что мы развиваемся, чтобы вам было удобно с нами работать.\
+        </p>\
+        <p class="undo-footer_p">\
+          Одинаковых заказов не бывает, и к каждому металлоизделию мы подходим с особой щепетильностью, учитывая всевозможные нюансы и тонкости.\
+        </p>\
+        <p class="undo-footer_p">\
+          В своей работе мы прежде всего отталкиваемся от Вашей заявки. Мы очень довольны собой, когда решаем Ваши проблемы, особенно, когда никто кроме нас не смог Вам помочь.\
+        </p>\
+      </div>';
