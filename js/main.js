f757892aaf466eefc9d4aa0702f6a4987bf7daa0
Runic Elf <perovrsq@gmail.com>
2018-04-03 10:10:55 +0000

    ref

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    //$('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">');
+    // $('meta[http-equiv=refresh]').remove();
      $('.slider').slick({
        autoplay:true,
        autoplaySpeed:5000,
