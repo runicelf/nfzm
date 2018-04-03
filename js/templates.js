b5eeeb0d05a3db2825bb4702e18a7964631dd4b1
Runic Elf <perovrsq@gmail.com>
2018-04-03 19:57:02 +0000

    ref

--- js/templates.js
+++ js/templates.js
@@ -65,6 +65,7 @@ const footer = '\
             <span class="footer_right_contacts_text footer_right_contacts_email">zakaz@nfzm.ru</span>\
           </div>\
         </div>\
+        <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter48047360 = new Ya.Metrika2({ id:48047360, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/48047360" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->\
       </div>';
 const undoFooter = '\
     <div class="undo-footer_wrapper">\
