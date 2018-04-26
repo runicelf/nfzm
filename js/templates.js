21fd28541ab7c23a511a4347653b76f43caedaf3
Runic Elf <perovrsq@gmail.com>
2018-04-26 12:44:05 +0000

    trg

--- js/templates.js
+++ js/templates.js
@@ -13,7 +13,7 @@ const header = '\
                         <li><a class="header_nav_link" href="../info/aboutus.html">О нас</a></li>\
                         <li><a class="header_nav_link" href="#">Производство</a></li>\
                         <li><a class="header_nav_link" href="#">Новости</a></li>\
-                        <li><a class="header_nav_link" href="#">Галерея</a></li>\
+                        <li><a class="header_nav_link" href="../info/gallery.html">Галерея</a></li>\
                         <li><a class="header_nav_link" href="../info/contacts.html">Контакты</a></li>\
                         <li><a class="header_nav_link" href="../info/vacancies.html">Вакансии</a></li>\
                     </ul>\
@@ -38,7 +38,7 @@ const headerMainPage = '\
                         <li><a class="header_nav_link" href="info/aboutus.html">О нас</a></li>\
                         <li><a class="header_nav_link" href="#">Новости</a></li>\
                         <li><a class="header_nav_link" href="#">Производство</a></li>\
-                        <li><a class="header_nav_link" href="#">Галерея</a></li>\
+                        <li><a class="header_nav_link" href="info/gallery.html">Галерея</a></li>\
                         <li><a class="header_nav_link" href="info/contacts.html">Контакты</a></li>\
                         <li><a class="header_nav_link" href="info/vacancies.html">Вакансии</a></li>\
                     </ul>\
